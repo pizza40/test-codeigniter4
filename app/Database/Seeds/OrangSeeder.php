92ba98dedabb7b177ff12f0ce759a03a04c815fb
pizza40 <ravi.attar.shahrulsyah@gmail.com>
2020-10-02 17:38:32 +0700

    use migration, seeder, and faker to make dummy

---
+++ app/Database/Seeds/OrangSeeder.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\I18n\Time;
+
+class OrangSeeder extends \CodeIgniter\Database\Seeder
+{
+    public function run()
+    {
+        // $data = [
+        //     [
+        //         'nama'          => 'Ravi Attar S',
+        //         'alamat'        => 'Jl.asdajwugyasgdy',
+        //         'created_at'    => Time::now(),
+        //         'updated_at'    => Time::now()
+        //     ],
+        //     [
+        //         'nama'          => 'Rot',
+        //         'alamat'        => 'Jl.asdajwugyasgdy',
+        //         'created_at'    => Time::now(),
+        //         'updated_at'    => Time::now()
+        //     ],
+        //     [
+        //         'nama'          => 'Mike',
+        //         'alamat'        => 'Jl.asdajwugyasgdy',
+        //         'created_at'    => Time::now(),
+        //         'updated_at'    => Time::now()
+        //     ],
+        // ];
+
+        $faker = \Faker\Factory::create('id_ID');
+        for($i = 0; $i < 100; $i++){
+            $data = [
+                'nama'          => $faker->name,
+                'alamat'        => $faker->address,
+                'created_at'    => Time::createFromTimestamp($faker->unixTime()),
+                'updated_at'    => Time::now()
+            ];
+            $this->db->table('orang')->insert($data);
+        }
+        
+        
+        // Simple Queries
+        // $this->db->query(
+        //     "INSERT INTO orang (nama, alamat, created_at, updated_at) VALUES(:nama:, :alamat:,
+        //     :created_at:, :updated_at:)",
+        //     $data
+        // );
+
+        // Using Query Builder
+        // $this->db->table('orang')->insertBatch($data);
+    }
+}
